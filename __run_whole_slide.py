cfd4340a4d9a4bc872db8cc5050ff43364bf5e68
Akaiko <58089998+Akaiko1@users.noreply.github.com>
2023-08-02 18:48:47 +0300

    Whole slide algorhitm updatet

--- __run_whole_slide.py
+++ __run_whole_slide.py
@@ -32,7 +32,10 @@ def main():
 
     contours, _ = cv2.findContours(preview_bin_gauss, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-    sample_size = (50, 50)
+    _ = plt.imshow(preview_bin_gauss)
+    plt.show()
+
+    sample_size = (25, 25)
     probes_bin = []
 
     for x in range(0, region_dimensions[0], sample_size[0]):
@@ -40,10 +43,13 @@ def main():
                 sample = preview_bin_gauss[y:y + sample_size[1], x:x + sample_size[0]]
                 normalized_sum = np.sum(sample/255)
 
-                if normalized_sum <= 700:
+                if normalized_sum <= 225:
                      preview_bin_gauss[y:y + sample_size[1], x:x + sample_size[0]] = 0
                 else:
                      probes_bin.append([x, y])
+    
+    _ = plt.imshow(preview_bin_gauss)
+    plt.show()
 
     # x,y,w,h = cv2.boundingRect(sorted(contours, key=lambda x: cv2.contourArea(x))[-1])
     # cv2.rectangle(preview_bin_gauss, (x,y), (x+w,y+h), 255, 5)
@@ -51,24 +57,26 @@ def main():
     probes_bin_rescaled = [[int(e * downsampling_coeff) for e in coords] for coords in probes_bin]
     print(len(probes_bin_rescaled))
 
-    for probe in [random.choice(probes_bin_rescaled)]:
+    selected_probes = []
+    for probe in random.sample(probes_bin_rescaled, 4):
         probe_image = slide.read_region((probe[0], probe[1]), 0, (int(sample_size[0] * downsampling_coeff), int(sample_size[1] * downsampling_coeff)))
         probe_image = np.array(probe_image)
         probe_image = cv2.cvtColor(probe_image, cv2.COLOR_RGBA2RGB)
         _ = plt.imshow(probe_image)
         plt.show()
+        selected_probes.append(probe_image)
         print(probe_image.shape)
     
     start = time.time()
+    print(f'Selected probes quantity: {len(selected_probes)}')
     resize_ops = tfs.ResizeOptions(chunk_size=(256, 256), model_input_size=(128, 128))
-    pathology_map = tfs.apply_segmentation_model_parallel([probe_image] * 4, endpoint_url='http://194.113.237.3:7500', model_name='demetra', batch_size=1,
+    pathology_map = tfs.apply_segmentation_model_parallel(selected_probes, endpoint_url='http://89.249.55.67:7500', model_name='demetra', batch_size=1,
                                                   resize_options=resize_ops, normalization=lambda x: x/255, parallelism_mode=1, thread_count=4)
-    pathology_map = np.asarray(ai.create_mask(pathology_map)[..., 0])[:probe_image.shape[0], :probe_image.shape[1]]
     print(f'Remote execution took {time.time() - start} seconds')
-    ai.display([probe_image, pathology_map], tensors=False)
 
-    _ = plt.imshow(preview_bin_gauss)
-    plt.show()
+    for idx, probe in enumerate(selected_probes):
+        map = np.asarray(ai.create_mask([pathology_map[idx]]))[:probe.shape[0], :probe.shape[1]]
+        ai.display([probe, map], tensors=False)
 
 
 if __name__ == '__main__':
